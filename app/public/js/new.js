66aa5d72a4befc66a8a7387b9eae8e17b441d892
Gcarthy <gerardwmc@gmail.com>
2021-10-20 14:36:13 -0400

    first commit

---
+++ app/public/js/new.js
@@ -0,0 +1,47 @@
+const New = {
+    data() {
+        return {
+            person: undefined,
+            books:[]
+        }
+    },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date).format("DD MMM YYYY")
+        }
+    },
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+                .then(response => response.json())
+                .then((responseJson) => {
+                    console.log(responseJson);
+                    this.person = responseJson.results[0];
+                })
+                .catch((err) => {
+                    console.error(err);
+                })
+        },
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$" + d;
+        },
+        fetchBookData() {
+            fetch('/api/book/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.books = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        }
+    },
+    created() {
+        this.fetchBookData();
+        this.fetchUserData();
+    }
+}
+
+Vue.createApp(New).mount('#App');
